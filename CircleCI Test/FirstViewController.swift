c2171a447cfb5a8abe22889d3d60316be144ed04
Evan Shortiss <evanshortiss@gmail.com>
2018-11-29 12:35:44 -0800

    Initial Commit

---
+++ CircleCI Test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CircleCI Test
+//
+//  Created by Evan Shortiss on 29/11/2018.
+//  Copyright © 2018 Evan Shortiss. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CircleCI Test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CircleCI Test
+//
+//  Created by Evan Shortiss on 29/11/2018.
+//  Copyright © 2018 Evan Shortiss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
